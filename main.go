acb8dea6c4f3e67385576e06e7564266a39c6a07
yaecho <root@yaecho.net>
2019-04-11 14:55:10 +0800

    format

--- main.go
+++ main.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 	"strconv"
 
+	"bytes"
 	"net"
 	"net/rpc"
-	"bytes"
 
 	"github.com/360EntSecGroup-Skylar/excelize"
 	"github.com/spiral/goridge"
@@ -29,12 +29,12 @@ func (s *Excel) Encode(data [][]interface{}, res *[]byte) error {
 	}
 
 	buf, err := xlsx.WriteToBuffer()
-	
+
 	if err != nil {
 		fmt.Println(err)
 		return nil
 	}
-	
+
 	*res = append(*res, buf.Bytes()...)
 
 	return nil
@@ -44,18 +44,18 @@ func (s *Excel) Encode(data [][]interface{}, res *[]byte) error {
 func (s *Excel) Decode(file []byte, res *[][]string) error {
 	buf := bytes.NewBuffer(file)
 	xlsx, err := excelize.OpenReader(buf)
-    if err != nil {
-        fmt.Println(err)
-        return err
-    }
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
 
-    // 获取 Sheet1 上所有单元格
+	// 获取 Sheet1 上所有单元格
 	rows, err := xlsx.GetRows("Sheet1")
 	if err != nil {
-        fmt.Println(err)
-        return err
+		fmt.Println(err)
+		return err
 	}
-	
+
 	*res = append(*res, rows...)
 
 	return nil
@@ -64,12 +64,12 @@ func (s *Excel) Decode(file []byte, res *[][]string) error {
 // 将数字列名转为大写字母
 func getColumnName(c int) (name string) {
 	for c >= 26 {
-		temp := c%26
-		name = string(temp + 65) + name
-		c = c/26-1;
+		temp := c % 26
+		name = string(temp+65) + name
+		c = c/26 - 1
 	}
-	name = string(65 + c) + name
-	
+	name = string(65+c) + name
+
 	return
 }
 
